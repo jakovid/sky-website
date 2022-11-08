b73cc4b60b58217bef91c62b2384e863d32908b9
jakovid <jacobk.dowdle@gmail.com>
2022-11-08 14:01:45 +0800

    added irene

--- src/data/TaiwanTeachers.js
+++ src/data/TaiwanTeachers.js
@@ -42,6 +42,11 @@ let TaiwanTeachers = [
         name: 'Mei-Jia',
         picture: meijiaPic,
         id: uuidv4()
+    },
+    {
+        name: 'Irene',
+        picture: null,
+        id: uuidv4()
     }
 ];
 
