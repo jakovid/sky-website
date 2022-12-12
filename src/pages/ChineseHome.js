3170228652b2a48d8769fca2f71c176138a4c4ef
jakovid <jacobk.dowdle@gmail.com>
2022-12-12 11:32:45 +0800

    added map to all pages

--- src/pages/ChineseHome.js
+++ src/pages/ChineseHome.js
@@ -13,6 +13,9 @@ export default function ChineseHome(){
         <ChineseHeroHome />
         <ChineseHomeBody />
         <ChineseHomeCTA />
+        <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3994.7906014735163!2d121.54842551500712!3d25.07943698395108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442ac0f05662dc7%3A0xc63b84f14b4d392f!2sSky%20Education!5e1!3m2!1sen!2stw!4v1670673646378!5m2!1sen!2stw" width="100%" height="300"  allowFullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        </div>
         <ChineseHomeLowerHero />
         <EnglishFooter />
         </>
