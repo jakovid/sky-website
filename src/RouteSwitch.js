7461bf19da586d983087d52a403d1a40252ce90f
Jacob Dowdle <jacobk.dowdle@gmail.com>
2022-12-13 19:49:45 +0800

    updated gh pages stuff

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -11,7 +11,7 @@ import ChineseOurClasses from "./pages/ChineseOurClasses";
 
 export default function RouteSwitch(){
     return(
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route path='/' element={<ChineseHome />} />
             <Route path='/about-us' element={<ChineseAboutUs />} />
