95104fbe568719fff6f0c7209112f53d62dbca74
Jacob Dowdle <jacobk.dowdle@gmail.com>
2022-12-27 15:34:55 +0800

    finished homepage mpbile stle

--- src/components/chinese/home/ChineseHomeCTA.js
+++ src/components/chinese/home/ChineseHomeCTA.js
@@ -6,8 +6,8 @@ export default function ChineseHomeCTA() {
     return(
         <div id='home-cta-container'>
             <div id='home-cta-text' className='chinese'>
-                <n className='number'>2023</n>年<n className='number'>1</n>月 <br />
-                開始最新課程！
+                <div><n className='number'>2023</n>年<n className='number'>1</n>月</div>
+                <div>開始最新課程！</div>
             </div>
 
             <div id='home-cta-email-wrapper'>
