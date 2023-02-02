3be06d27e77ae6cf0c61b558dc89c039516de08c
jakovid <jacobk.dowdle@gmail.com>
2023-02-02 10:13:24 +0800

    updated header style

--- src/components/chinese/ChineseHeader.js
+++ src/components/chinese/ChineseHeader.js
@@ -2,6 +2,12 @@ import React from "react";
 import '../../styles/Header.css';
 import USAflag from '../../images/USAflag.svg';
 import SkyLogo from '../../images/SkyLogo.webp';
+import Home from "../../images/icons/home.png";
+import AboutUs from "../../images/icons/about-us.png";
+import OurTeam from "../../images/icons/our-team.png";
+import OurClasses from "../../images/icons/our-classes.png";
+import ContactUs from "../../images/icons/contact-us.png";
+
 
 export default function ChineseHeader() {
     return(
@@ -10,11 +16,27 @@ export default function ChineseHeader() {
                 <div id='header-wrapper-1'>
                     <img src={SkyLogo} id='header-logo' />
                     <div id='header-nav'>
-                        <a href='../sky-website/' className='header-btn chinese'>主頁</a>
-                        <a href='../sky-website/#/about-us' className='header-btn chinese'>關於我們</a>
-                        <a href='../sky-website/#/our-team' className='header-btn chinese'>我們的團隊</a>
-                        <a href='../sky-website/#/our-classes' className='header-btn chinese'>我們的課程</a>
-                        <a href='../sky-website/#/contact-us' className='header-btn chinese'>聯繫我們</a>
+                        <a href='../sky-website/' title="主頁">
+                            <div className='header-btn chinese'>主頁</div>
+                            <img className="header-btn-icon" src={Home}/>
+                        </a>
+                        <a href='../sky-website/#/about-us' title="關於我們" >
+                            <div className='header-btn chinese'>關於我們</div>
+                            <img className="header-btn-icon" src={AboutUs}/>
+                        </a>
+                        <a href='../sky-website/#/our-team' title="我們的團隊">
+                            <div className='header-btn chinese'>我們的團隊</div>
+                            <img className="header-btn-icon" src={OurTeam}/>
+                        </a>
+                        <a href='../sky-website/#/our-classes' title="我們的課程" >
+                            <div className='header-btn chinese'>我們的課程</div>
+                            <img className="header-btn-icon" src={OurClasses}/>
+                        </a>
+                        <a href='../sky-website/#/contact-us' title="聯繫我們">
+                            <div className='header-btn chinese'>聯繫我們</div>
+                            <img className="header-btn-icon" src={ContactUs}/>
+                        </a>
+
                     </div>
                     <div>
                         <a href='../sky-website/#/home/en' id='header-language'>
