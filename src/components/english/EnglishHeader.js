fdbd509959f721cc0baf592786794216f2021c93
jakovid <jacobk.dowdle@gmail.com>
2023-02-02 10:02:30 +0800

    updated header style for laptop and mobile

--- src/components/english/EnglishHeader.js
+++ src/components/english/EnglishHeader.js
@@ -2,6 +2,11 @@ import React from "react";
 import '../../styles/Header.css';
 import TWflag from '../../images/TWFlag.png';
 import SkyLogo from '../../images/SkyLogo.webp';
+import Home from "../../images/icons/home.png";
+import AboutUs from "../../images/icons/about-us.png";
+import OurTeam from "../../images/icons/our-team.png";
+import OurClasses from "../../images/icons/our-classes.png";
+import ContactUs from "../../images/icons/contact-us.png";
 
 export default function EnglishHeader() {
     return(
@@ -10,16 +15,31 @@ export default function EnglishHeader() {
                 <div id='header-wrapper-1'>
                     <img src={SkyLogo} id='header-logo' />
                     <div id='header-nav'>
-                        <a href='../sky-website/#/home/en' className='header-btn english'>Home</a>
-                        <a href='../sky-website/#/about-us/en' className='header-btn english'>About Us</a>
-                        <a href='../sky-website/#/our-team/en' className='header-btn english'>Our Team</a>
-                        <a href='../sky-website/#/our-classes/en' className='header-btn english'>Our Classes</a>
-                        <a href='../sky-website/#/contact-us/en' className='header-btn english'>Contact Us</a>
+                        <a href='../sky-website/#/home/en' title="Home">
+                            <div className='header-btn english'>Home</div>
+                            <img className="header-btn-icon" src={Home}/>
+                        </a>
+                        <a href='../sky-website/#/about-us/en' title="About Us" >
+                            <div className='header-btn english'>About Us</div>
+                            <img className="header-btn-icon" src={AboutUs}/>
+                        </a>
+                        <a href='../sky-website/#/our-team/en' title="Our Team">
+                            <div className='header-btn english'>Our Team</div>
+                            <img className="header-btn-icon" src={OurTeam}/>
+                        </a>
+                        <a href='../sky-website/#/our-classes/en' title="Our Classes" >
+                            <div className='header-btn english'>Our Classes</div>
+                            <img className="header-btn-icon" src={OurClasses}/>
+                        </a>
+                        <a href='../sky-website/#/contact-us/en' title="Contact Us">
+                            <div className='header-btn english'>Contact Us</div>
+                            <img className="header-btn-icon" src={ContactUs}/>
+                        </a>
                     </div>
                     <div>
                         <a href='../sky-website/' id='header-language'>
-                        <img class='header-language-flag' src={TWflag} />
-                        <div>中文</div>
+                            <img class='header-language-flag' src={TWflag} />
+                            <div class='header-language-name'>中文</div>
                         </a>
                     </div>
                 </div>
