e974ede8fd43412d0e44251e89fea57cfe3fd039
jakovid <jacobk.dowdle@gmail.com>
2023-03-26 22:36:20 +0800

    updated a few things

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import RouteSwitch from './RouteSwitch';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import EnglishHome from "./pages/EnglishHome";
